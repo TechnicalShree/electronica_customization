584f03d93c81818db6cd89d3d2e51f119d8f4b11
TechnicalShree <krushnasr96@gmail.com>
2025-01-13 19:50:36 +0000

    feat: Add new document models and associated test cases for Item, Item Series, Account Type, Material Type, and Item Main Selection

---
+++ electronica_customization/public/js/item.js
@@ -0,0 +1,40 @@
+frappe.ui.form.on("Item", {
+    custom_main_selection: (frm) => {
+        handleMainSelectionUpdate(frm);
+    },
+    custom_series: async (frm) => {
+        handleSeriesUpdate(frm);
+    },
+});
+
+
+async function handleMainSelectionUpdate(frm) {
+    frm.fields_dict.custom_series.get_query = function (doc) {
+        return {
+            filters: [
+                ['main_selection', '=', frm.doc.custom_main_selection]
+            ]
+        };
+    };
+
+    frm.set_value("custom_series", "");
+    frm.set_value("custom_model", "");
+
+    frm.refresh_field("custom_series");
+    frm.refresh_field("custom_model");
+}
+
+async function handleSeriesUpdate(frm) {
+    frm.fields_dict.custom_model.get_query = function (doc) {
+        return {
+            filters: [
+                ['main_selection', '=', frm.doc.custom_main_selection],
+                ['series', '=', frm.doc.custom_series]
+            ]
+        };
+    };
+
+    frm.set_value("custom_model", "");
+
+    frm.refresh_field("custom_model");
+}
