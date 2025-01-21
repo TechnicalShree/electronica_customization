cd39b611f99e96ae2f36b901efe1f57a8cdabf43
TechnicalShree <krushnasr96@gmail.com>
2025-01-21 18:59:07 +0000

    feat: implement get_primary_address API and update Maintenance Visit form to auto-fill customer addresses

--- electronica_customization/public/js/maintenance_visit.js
+++ electronica_customization/public/js/maintenance_visit.js
@@ -2,7 +2,39 @@ frappe.ui.form.on('Maintenance Visit', {
     setup: (frm) => {
         handleApplyFiltersToAddress(frm);
     },
+    refresh: (frm) => {
+        addDefaultAddress(frm);
+    },
+    customer: (frm) => {
+        addDefaultAddress(frm);
+    }
 });
+
+async function addDefaultAddress(frm){
+    if(!frm.doc.customer){
+        frm.set_value('custom_customer_billing_address', "");
+        frm.set_value('custom_customer_shipping_address', "");
+
+        frm.refresh_field('custom_customer_billing_address');
+        frm.refresh_field('custom_customer_shipping_address');
+        return;
+    }
+
+    const address_info = await frappe.call({
+        method: 'electronica_customization.api.engineer_visit.get_primary_address',
+        args: {
+            customer: frm.doc.customer,
+        },
+    })
+
+    if(address_info?.message){
+        frm.set_value('custom_customer_billing_address', address_info.message?.primary_billing_address || "");
+        frm.set_value('custom_customer_shipping_address', address_info.message?.primary_shipping_address || "");
+
+        frm.refresh_field('custom_customer_billing_address');
+        frm.refresh_field('custom_customer_shipping_address');
+    }
+}
 
 function handleApplyFiltersToAddress(frm){
     frm.fields_dict.custom_customer_billing_address.get_query = function (doc) {
@@ -15,7 +47,7 @@ function handleApplyFiltersToAddress(frm){
         };
     };
 
-    frm.fields_dict.custom_customer_shiping_address.get_query = function (doc) {
+    frm.fields_dict.custom_customer_shipping_address.get_query = function (doc) {
         return {
             filters: [
                 ['address_type', '=', 'Shipping'],
