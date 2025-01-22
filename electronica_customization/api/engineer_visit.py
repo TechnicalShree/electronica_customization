1630180b97da8880fa6c8fc1c26407fedb02f687
TechnicalShree <krushnasr96@gmail.com>
2025-01-22 13:38:16 +0000

    fix: improve address retrieval logic in get_primary_address function

--- electronica_customization/api/engineer_visit.py
+++ electronica_customization/api/engineer_visit.py
@@ -10,6 +10,9 @@ def get_primary_address(customer):
         ["address_type", "=", "Shipping"],
         ["is_shipping_address", "=", "1"],
     ]
+    if not frappe.db.exists("Address", shipping_filters):
+        del shipping_filters[-1]
+
     primary_shipping_address = frappe.get_value("Address", shipping_filters, "name")
 
     billing_filters = [
@@ -18,17 +21,11 @@ def get_primary_address(customer):
         ["address_type", "=", "Billing"],
         ["is_primary_address", "=", "1"],
     ]
-    primary_billing_address = frappe.get_value("Address", billing_filters, "name")
 
-    frappe.logger("debug").error(
-        [
-            "-------------------",
-            shipping_filters,
-            primary_shipping_address,
-            billing_filters,
-            primary_billing_address,
-        ]
-    )
+    if not frappe.db.exists("Address", billing_filters):
+        del billing_filters[-1]
+
+    primary_billing_address = frappe.get_value("Address", billing_filters, "name")
 
     return {
         "primary_shipping_address": primary_shipping_address,
