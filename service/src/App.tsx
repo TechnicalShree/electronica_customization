cab5c575af93676f80b1543f55e14d26aa9e0dd5
TechnicalShree <krushnasr96@gmail.com>
2025-02-11 19:59:14 +0530

    feat: add new doctype files for Modul Trail, Problem Observed, Product Category, and Product Sub Category; include related JavaScript and Python test files; update package.json to include formiojs dependency; add FontAwesome assets and CSS links in HTML files

--- service/src/App.tsx
+++ service/src/App.tsx
@@ -1,14 +1,13 @@
 import { FrappeProvider } from "frappe-react-sdk";
 import "chatnext-ui/dist/index";
+import "formiojs/dist/formio.full.min.css";
 import "chatnext-ui/dist/index.css";
-import Dashboard from "./components/Page/Dashboard";
 import AppRouter from "./AppRouter";
 
 function App() {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <FrappeProvider>
-        <Dashboard />
         <AppRouter />
         {/* @ts-ignore */}
         <chatnext-app></chatnext-app>
