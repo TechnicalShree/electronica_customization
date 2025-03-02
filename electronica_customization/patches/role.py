d7632f9456f71cc86bcf720f53b5276ea996ed45
TechnicalShree <krushnasr96@gmail.com>
2025-03-02 13:23:09 +0530

    feat: add role management patch to create Factory User, HO, and Branch Manager roles

---
+++ electronica_customization/patches/role.py
@@ -0,0 +1,44 @@
+import frappe
+
+
+def execute():
+    add_roles()
+
+def add_roles():
+    roles = [
+        {
+            "role_name":"Factory User",
+        },
+        {
+            "role_name":"HO",
+        },
+        {
+            "role_name":"Branch Manager",
+        },
+    ]
+
+    for role in roles:
+        try:
+            # Check if Role already exists
+            if not frappe.db.exists(
+                "Translation",
+                {
+                    "role_name": role["role_name"],
+                },
+            ):
+                # Create and save the new Role document
+                role_doc = frappe.get_doc(
+                    {
+                        "doctype": "Role",
+                        "role_name": role["role_name"],
+                    }
+                )
+                role_doc.save(ignore_permissions=True)
+                frappe.db.commit()  # Commit the transaction to ensure the save is applied
+                frappe.logger().info(
+                    f"Added role: {role['role_name']} -> {role['role_name']}"
+                )
+        except Exception as e:
+            frappe.logger("patch_exception").exception(
+                f"Error adding role: {role}"
+            )
